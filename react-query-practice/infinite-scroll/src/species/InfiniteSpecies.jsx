e66abd43f5b33e06a45376dd1bcd8eb63cf8c73a
kbu715 <kbu715@gmail.com>
2022-08-28 23:49:00 +0900

    react-query-practice: Implement another infinite scroll component

--- react-query-practice/infinite-scroll/src/species/InfiniteSpecies.jsx
+++ react-query-practice/infinite-scroll/src/species/InfiniteSpecies.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
@@ -9,5 +10,38 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
+    "sw-species",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error! ${error.toString()}</div>;
+  return (
+    <>
+      {isFetching && <div className="loading">Fetching...</div>}
+      <InfiniteScroll hasMore={hasNextPage} loadMore={fetchNextPage}>
+        {data.pages.map((pageData) =>
+          pageData.results.map((kind) => (
+            <Species
+              key={kind.name}
+              name={kind.name}
+              language={kind.language}
+              averageLifespan={kind.average_lifespan}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </>
+  );
 }
