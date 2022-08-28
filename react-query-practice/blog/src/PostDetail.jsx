3cfc2bdfdc23b7fefc41bf8111e89f56be61917a
kbu715 <kbu715@gmail.com>
2022-08-28 18:41:19 +0900

    react-query-practice: 쿼리 키 (의존성배열)

--- react-query-practice/blog/src/PostDetail.jsx
+++ react-query-practice/blog/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,8 +25,20 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(
+    // 쿼리 키를 쿼리에 대한 의존성 배열로 취급합니다.
+    ["postComments", post.id],
+    () => fetchComments(post.id)
+  );
 
+  if (isLoading) return <div>Loading...</div>;
+  if (isError)
+    return (
+      <>
+        <h3>Oops, something went wrong!</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
