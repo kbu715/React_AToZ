11e12017c48a075e8398ab383b7ee0b3b53266b0
kbu715 <kbu715@gmail.com>
2022-09-04 23:15:25 +0900

    Add useStaff custom hook

--- react-query-practice/full-stack-app/client/src/react-query/queryClient.ts
+++ react-query-practice/full-stack-app/client/src/react-query/queryClient.ts
@@ -7,13 +7,13 @@ const toast = createStandaloneToast({ theme });
 
 function queryErrorHandler(error: unknown): void {
   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-  const id = 'react-query-error';
+  // const id = `react-query-error@${new Date()}`;
   const title =
     error instanceof Error ? error.message : '서버 연결중 에러 발생!';
 
   // prevent duplicate toasts
   toast.closeAll();
-  toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+  toast({ title, status: 'error', variant: 'subtle', isClosable: true });
 }
 
 export const queryClient = new QueryClient({
